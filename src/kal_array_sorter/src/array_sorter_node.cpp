f2e70051a2cb2df14f62b72a2de847958ece7937
davidkalmn <kaldavai26@gmail.com>
2024-10-13 22:47:14 +0200

    Array sorter kodstrukturajanak modositasa, hogy atlathatobb legyen

--- src/kal_array_sorter/src/array_sorter_node.cpp
+++ src/kal_array_sorter/src/array_sorter_node.cpp
@@ -3,52 +3,56 @@
 #include <vector>
 #include <algorithm>
 
-using std::placeholders::_1;
-
 class ArraySorter : public rclcpp::Node
 {
 public:
-    ArraySorter() : Node("array_sorter")
-    {
-        array_sub = this->create_subscription<std_msgs::msg::Float64MultiArray>(
-            "unsorted_array", 10, 
-            std::bind(&ArraySorter::unsorted_array_callback, this, _1));
+  ArraySorter() : Node("array_sorter")
+  {
+    array_sub = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+        "unsorted_array", 10, std::bind(&ArraySorter::process_unsorted_array, this, std::placeholders::_1));
 
-        array_pub = this->create_publisher<std_msgs::msg::Float64MultiArray>("sorted_array", 1);
-    }
+    array_pub = this->create_publisher<std_msgs::msg::Float64MultiArray>("sorted_array", 1);
+  }
 
-    void unsorted_array_callback(const std_msgs::msg::Float64MultiArray& unsorted_array)
-    {
-        sorted_array.data.clear();
-        
-        sorted_array.data = unsorted_array.data;
-        
-        std::sort(sorted_array.data.begin(), sorted_array.data.end());
+private:
+  void process_unsorted_array(const std_msgs::msg::Float64MultiArray::SharedPtr unsorted_array_msg)
+  {
+    std::vector<double> array_data = unsorted_array_msg->data;
 
-        array_pub->publish(sorted_array);
-    }
+    sort_array(array_data);
 
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr array_pub;
-    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr array_sub;
-    
+    publish_sorted_array(array_data);
+  }
+
+  void sort_array(std::vector<double>& data)
+  {
+    std::sort(data.begin(), data.end());
+  }
+
+  void publish_sorted_array(const std::vector<double>& sorted_data)
+  {
     std_msgs::msg::Float64MultiArray sorted_array;
+    sorted_array.data = sorted_data;
+    this->array_pub->publish(sorted_array);
+  }
+
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr array_pub;
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr array_sub;
 };
 
 int main(int argc, char** argv)
 {
-    rclcpp::init(argc, argv);
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ArraySorter>();
 
-    auto node = std::make_shared<ArraySorter>();
+  rclcpp::Rate rate(10);
 
-    rclcpp::Rate rate(0.5);
+  while (rclcpp::ok())
+  {
+    rclcpp::spin_some(node);
+    rate.sleep();
+  }
 
-    while (rclcpp::ok())
-    {
-        rclcpp::spin_some(node);
-        
-        rate.sleep();
-    }
-
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::shutdown();
+  return 0;
 }
